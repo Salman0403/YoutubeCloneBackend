deccf329b9b12e2db2ccbd3a385421ed56b472b7
Salman0403 <muhammadsalman0403@gmail.com>
2024-03-13 12:24:07 +0500

    fix: asynchandler function import app file error and create first route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express  from "express";
 import cors from 'cors'
 import cookieParser from "cookie-parser";
-
 const app = express()
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials:true
@@ -11,5 +11,12 @@ app.use(cors({
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended: true, limit:'16kb'}))
 app.use(express.static("public"))
-app.use(express.cookieParser())
-export default app
+app.use(cookieParser())
+
+// import routes
+import userRouter from './routes/user.routes.js'
+
+// declared routes
+app.use("/users",userRouter )
+
+export {app}
